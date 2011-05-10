e80009724ec81d9e456069b94a832f1ed52c8611
Maikel Alderhout <malderhout@gmail.com>
2011-05-10 12:56:00 -0700

    Edited grails-app/domain/knowledgematch/Kandidaat.groovy via GitHub

--- grails-app/domain/knowledgematch/Kandidaat.groovy
+++ grails-app/domain/knowledgematch/Kandidaat.groovy
@@ -7,10 +7,6 @@ class Kandidaat {
 	String Adres
 	String Woonplaats
 	String Telefoon
-	String Telefoon2
-        String Telefoon3
-        String Telefoon4
-        String Telefoon5
         Date   Geboortedatum
 	
     static constraints = {
